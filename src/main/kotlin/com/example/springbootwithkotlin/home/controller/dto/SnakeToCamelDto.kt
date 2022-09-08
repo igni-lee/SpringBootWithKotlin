9a27bd4b81a983941aeb6c9bf753e5a8f508440e
igni <igni.lee.dev@gmail.com>
2022-09-08 14:36:57 +0900

    gitignore 재적용

--- src/main/kotlin/com/example/springbootwithkotlin/home/controller/dto/SnakeToCamelDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.springbootwithkotlin.home.controller.dto
-
-data class SnakeToCamelDto(
-    val token: String?,
-    val accessToken: String?,
-    val refreshToken: String?,
-)

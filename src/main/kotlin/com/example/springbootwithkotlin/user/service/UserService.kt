046ae934805f74cf10c5828fab857ab1efd531f6
igni <igni.lee.dev@gmail.com>
2022-09-08 14:31:19 +0900

    트랜잭션 적용

--- src/main/kotlin/com/example/springbootwithkotlin/user/service/UserService.kt
+++ src/main/kotlin/com/example/springbootwithkotlin/user/service/UserService.kt
@@ -4,13 +4,12 @@ import com.example.springbootwithkotlin.user.dto.SignupDto
 import com.example.springbootwithkotlin.user.entity.UserEntity
 import com.example.springbootwithkotlin.user.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserService(
     val userRepository: UserRepository,
 ) {
-    fun signup(signupDto: SignupDto) {
-        val newUser = UserEntity.from(signupDto)
-        userRepository.save(newUser)
-    }
+    @Transactional
+    fun signup(signupDto: SignupDto) = userRepository.save(UserEntity.from(signupDto))
 }

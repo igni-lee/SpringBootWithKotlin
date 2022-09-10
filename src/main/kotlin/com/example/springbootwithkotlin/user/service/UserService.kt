859e5162a33d249ad63b12be9d48ed1ce3c50c67
igni <igni.lee.dev@gmail.com>
2022-09-10 23:05:57 +0900

    이메일 중복 예외처리
    - 이메일 중복시 회원가입 되지 않도록 예외처리
    - test 패키지에 있는 i18n 삭제
    - 테스트코드 추가
    - SignupException을 추가하고, 해당 예외 발생시 UserControllerAdvice에서 처리하도록 개발

--- src/main/kotlin/com/example/springbootwithkotlin/user/service/UserService.kt
+++ src/main/kotlin/com/example/springbootwithkotlin/user/service/UserService.kt
@@ -2,7 +2,9 @@ package com.example.springbootwithkotlin.user.service
 
 import com.example.springbootwithkotlin.user.dto.SignupDto
 import com.example.springbootwithkotlin.user.entity.UserEntity
+import com.example.springbootwithkotlin.user.exception.SignupException
 import com.example.springbootwithkotlin.user.repository.UserRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,5 +13,11 @@ class UserService(
     val userRepository: UserRepository,
 ) {
     @Transactional
-    fun signup(signupDto: SignupDto) = userRepository.save(UserEntity.from(signupDto))
+    fun signup(signupDto: SignupDto) {
+        userRepository.findByEmail(signupDto.email).ifPresent {
+            throw SignupException(HttpStatus.CONFLICT, "email_duplicate")
+        }
+
+        userRepository.save(UserEntity.from(signupDto))
+    }
 }
